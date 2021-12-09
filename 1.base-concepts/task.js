ecfab00e826dbbcac3657e2cecac13ec2df79625
Олег А. Атласов <atlasovoa@kchadm.ru>
2021-12-09 11:04:43 +0400

    Task solution #1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,41 @@
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let D = b ** 2 - 4 * a * c;
+  if (D < 0) {
+    arr = [];
+  } else {
+    if (D === 0) {
+      arr = [-b / (2 * a)];
+    } else {
+      if (D > 0) {
+        arr = [(-b + Math.sqrt(D)) / (2 * a), (-b - Math.sqrt(D)) / (2 * a)];
+      }
+    }
+  }
+  return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+// function monthDiff(date, now) {
+//   let n;
+//   now = new Date();
+//   date = new Date();
+//   n = (date.getFullYear() - now.getFullYear()) * 12;
+//   n -= date.getMonth();
+//   n += now.getMonth();
+//   return n;
+// }
 
-  // код для задачи №2 писать здесь
+function calculateTotalMortgage(percent, contribution, amount, date) {
+  date = new Date();
+  let n = (date.getMilliseconds() - Date.now()) / 2678400000;
+  let S = amount - contribution;
+  let P = percent/12/100;
+  let monthPayment = S * (P + (P / ((Math.pow((1 + P), n)) - 1)));
+
+  let totalAmount =  Math.floor(monthPayment * n * 100) / 100;
 
   return totalAmount;
 }
+console.log(calculateTotalMortgage(percent, contribution, amount, date))
