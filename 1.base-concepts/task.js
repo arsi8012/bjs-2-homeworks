22dbe7ddf32c709c7ab58c59bd68ed16fdb17a27
Олег А. Атласов <atlasovoa@kchadm.ru>
2021-12-09 11:35:26 +0400

    Edited task1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,18 +1,13 @@
 "use strict";
 
 function solveEquation(a, b, c) {
-  let arr;
-  let D = b ** 2 - 4 * a * c;
-  if (D < 0) {
-    arr = [];
-  } else {
-    if (D === 0) {
-      arr = [-b / (2 * a)];
-    } else {
-      if (D > 0) {
-        arr = [(-b + Math.sqrt(D)) / (2 * a), (-b - Math.sqrt(D)) / (2 * a)];
-      }
-    }
+  let arr = [];
+  let d = b ** 2 - 4 * a * c;
+  if (d === 0) {
+    arr = [-b / (2 * a)];
+  }
+  if (d > 0) {
+    arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
   }
   return arr;
 }
@@ -31,10 +26,10 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   date = new Date();
   let n = (date.getMilliseconds() - Date.now()) / 2678400000;
   let S = amount - contribution;
-  let P = percent/12/100;
+  let P = percent / 12 / 100;
   let monthPayment = S * (P + (P / ((Math.pow((1 + P), n)) - 1)));
 
-  let totalAmount =  Math.floor(monthPayment * n * 100) / 100;
+  let totalAmount = Math.floor(monthPayment * n * 100) / 100;
 
   return totalAmount;
 }
