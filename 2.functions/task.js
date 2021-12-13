dfdebe8bcd0d1a8ece4a095dde28df8f2d6dfd83
Олег А. Атласов <atlasovoa@kchadm.ru>
2021-12-13 10:27:37 +0400

    Function

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,58 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
 
-  // Ваш код
+  let min = 5;
+  let max = 5;
+  let sum = 0;
+  let avg = 0;
 
-  return { min: min, max: max, avg: avg };
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    sum += arr[i];
+  }
+
+  avg = Number((sum / arr.length).toFixed(2));
+
+  return { min, max, avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
-  return max;
+  let max = 0;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    if (func(arrOfArr[i]) > max) {
+      max = func(arrOfArr[i]);
+    }
+    return max;
+  }
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = 0;
+  let max = 0;
+  let dist = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      arr[i] = max;
+    }
+    dist = Math.abs(max - min);
+  }
+  return dist;
 }
