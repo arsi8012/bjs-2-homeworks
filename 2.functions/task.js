23433c79179abc0499d548df97a19244bb5f73dd
Олег А. Атласов <atlasovoa@kchadm.ru>
2021-12-15 16:16:00 +0400

    Arrays

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,8 +1,8 @@
 // Задание 1
 function getArrayParams(arr) {
 
-  let min = 5;
-  let max = 5;
+  let min = 100;
+  let max = -100;
   let sum = 0;
   let avg = 0;
 
@@ -33,8 +33,9 @@ function worker(arr) {
 function makeWork(arrOfArr, func) {
   let max = 0;
   for (let i = 0; i < arrOfArr.length; i++) {
-    if (func(arrOfArr[i]) > max) {
-      max = func(arrOfArr[i]);
+    const result = func(arrOfArr[i]);
+    if (result > max) {
+      max = result;
     }
     return max;
   }
@@ -44,7 +45,6 @@ function makeWork(arrOfArr, func) {
 function worker2(arr) {
   let min = 0;
   let max = 0;
-  let dist = 0;
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] < min) {
       min = arr[i];
@@ -52,7 +52,6 @@ function worker2(arr) {
     if (arr[i] > max) {
       arr[i] = max;
     }
-    dist = Math.abs(max - min);
   }
-  return dist;
+  return Math.abs(max - min);
 }
--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,17 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  result = arr1.length === arr2.length && arr1.every((item, i) => item === arr2[i]);
 
-  return result; // boolean
+  return result;
 }
 
-function advancedFilter(arr) {
-  let resultArr;
 
-  // Ваш код
+
+
+function advancedFilter(arr) {
+
+  let resultArr = arr.filter(item => item > 0).filter(item => item % 3 === 0).map(item => item * 10);
 
   return resultArr; // array
 }
