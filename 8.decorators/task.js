4d0659e680622934140d3adc12c39b5268015664
Олег А. Атласов <atlasovoa@kchadm.ru>
2022-01-17 08:16:31 +0400

    Decorator_3

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -3,12 +3,12 @@ function cachingDecoratorNew(func) {
 
   function wrapper(...args) {
     const hash = args.join(',');
-    let result = cache.filter(item => item.hash === hash);
-    if (result.length === 1) {
-      console.log("Из кэша: " + result[0].value);
-      return "Из кэша: " + result[0].value;
+    let result = cache.find(item => item.hash === hash);
+    if (result) {
+      console.log("Из кэша: " + result.value);
+      return "Из кэша: " + result.value;
     } else {
-      let value = func(...args);
+      let value = func.call(this, ...args);
       if (cache.length > 5) {
         cache.shift();
       }
@@ -29,7 +29,6 @@ function debounceDecoratorNew(func, ms) {
     if (nextCall) {
       func.apply(this, ...args);
       nextCall = false;
-      return;
     }
     clearTimeout(interval);
     interval = setTimeout(() => {
@@ -40,13 +39,22 @@ function debounceDecoratorNew(func, ms) {
   return wrapper;
 }
 
-function debounceDecorator2(func) {
+function debounceDecorator2(func, ms) {
+  let nextCall = true;
+  let interval;
   wrapper.count = 0;
 
   function wrapper(...args) {
-    wrapper.history.push(args);
     wrapper.count++;
-    return func.call(this, ...args);
+    if (nextCall) {
+      func.apply(this, ...args);
+      nextCall = false;
+    }
+    clearTimeout(interval);
+    interval = setTimeout(() => {
+      func.apply(this, ...args);
+      nextCall = true;
+    }, ms)
   }
   return wrapper;
 }
